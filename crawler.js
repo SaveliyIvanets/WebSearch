33d1cd9858be41534d740ca208286f7e39bd014a
Saveliy Ivanets <ivanets300@mail.ru>
2026-07-13 23:52:05 +0300

    refactoring the crawler module

--- crawler.js
+++ crawler.js
@@ -8,43 +8,64 @@ const DELAY_MS = 1000;
 
 async function crawl(startUrl, maxPages = 20) {
   queue.push(startUrl);
+  const startUrlObj = new URL(startUrl);
+  const baseDomain = startUrlObj.hostname;
   while (queue.length !== 0 && visited.size < maxPages) {
     const currentUrl = queue.shift();
-    const baseDomain = new URL(currentUrl).hostname;
+    if (visited.has(currentUrl)) continue; // for future work when there will be several workers
     visited.add(currentUrl);
-    let response;
-    try {
-      response = await axios.get(currentUrl, {
-        timeout: 10000,
-        headers: { "User-Agent": "MySearchBot/1.0" },
-      });
-    } catch (err) {
+    const html = await _fetchHtml(currentUrl);
+    if (!html) {
       visited.delete(currentUrl);
       continue;
     }
-    const html = response.data;
-    const $ = cheerio.load(html);
-    const allText = $("body").text();
+    const { $, allText } = _extractPageData(html);
     addDocument(currentUrl, allText);
-    getInternalLinks($, currentUrl, baseDomain).forEach((item) => {
-      if (!visited.has(item) && !queue.includes(item)) {
-        queue.push(item);
+    const foundLinks = _extractInternalLinks($, currentUrl, baseDomain);
+    for (const link of foundLinks) {
+      if (!visited.has(link) && !queue.includes(link)) {
+        queue.push(link);
       }
-    });
+    }
     await new Promise((resolve) => setTimeout(resolve, DELAY_MS));
   }
 }
-function getInternalLinks($, currentUrl, baseDomain) {
+function _extractInternalLinks($, currentUrl, baseDomain) {
   const internalLinks = [];
-  const baseUrl = new URL(currentUrl);
   $("a").each((i, el) => {
     const href = $(el).attr("href");
     if (!href) return;
-    if (href.startsWith("#") || href.startsWith("mailto:")) return;
-    const absoluteUrl = new URL(href, currentUrl);
-    if (absoluteUrl.hostname === baseDomain) {
-      internalLinks.push(absoluteUrl.href);
-    }
+    if (
+      href.startsWith("#") ||
+      href.startsWith("mailto:") ||
+      href.startsWith("javascript:")
+    )
+      return;
+    try {
+      const absoluteUrl = new URL(href, currentUrl);
+      if (absoluteUrl.hostname === baseDomain) {
+        internalLinks.push(absoluteUrl.href);
+      }
+    } catch (err) {}
   });
   return internalLinks;
 }
+
+async function _fetchHtml(url) {
+  let response;
+  try {
+    response = await axios.get(url, {
+      timeout: 10000,
+      headers: { "User-Agent": "SearchBot/1.0" },
+    });
+    return response.data;
+  } catch (err) {
+    return null;
+  }
+}
+function _extractPageData(html) {
+  const $ = cheerio.load(html);
+  const allText = $("body").text();
+  return { $, allText };
+}
+module.exports = { crawl };
