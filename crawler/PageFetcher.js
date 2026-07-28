b91f3085164a8298170798c32e6f166c7f97b0cf
Saveliy Ivanets <ivanets300@mail.ru>
2026-07-28 21:26:09 +0300

    created a page fetcher: refactoring the structure

---
+++ crawler/PageFetcher.js
@@ -0,0 +1,31 @@
+const axios = require("axios");
+class PageFetcher {
+  constructor(options = {}) {
+    this.timeout = options.timeout ?? 10000;
+    this.userAgent = options.userAgent ?? "SearchBot/1.0";
+  }
+  async fetchText(url) {
+    try {
+      const response = await axios.get(url, {
+        timeout: this.timeout,
+        responseType: "text",
+        headers: {
+          Accept: "text/html, text/plain",
+          "User-Agent": this.userAgent,
+        },
+      });
+      const contentType = response.headers["content-type"]?.toLowerCase() || "";
+      if (
+        contentType.includes("text/html") ||
+        contentType.includes("text/plain")
+      ) {
+        return response.data;
+      }
+      return null;
+    } catch (err) {
+      return null;
+    }
+  }
+}
+
+module.exports = { Fetcher };
