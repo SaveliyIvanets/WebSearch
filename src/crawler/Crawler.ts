7de403efe1975e4c31535af7838ea99e2f053725
Dinarchak <jjustdinar@gmail.com>
2026-08-16 14:45:18 +0300

    rewrite Crawler.ts

---
+++ src/crawler/Crawler.ts
@@ -0,0 +1,88 @@
+import { Worker } from "./Worker.js";
+import { Queue } from "./types/Queue.js";
+import { CrawlTask } from "./types/CrawlTask.js";
+import { State } from "./types/State.js";
+
+interface Options {
+  workerCount?: number;
+
+  pageFetcher: any;
+  htmlParser: any;
+  linkExtractor: any;
+  robotsParser: any;
+  indexStore: any;
+
+  maxPages?: number;
+  maxDepth?: number;
+}
+
+class Crawler {
+  private readonly workerCount: number;
+
+  private readonly queue: Queue<CrawlTask>;
+  private readonly visited: Set<string>;
+  private readonly state: State;
+
+  private readonly pageFetcher: any;
+  private readonly htmlParser: any;
+  private readonly linkExtractor: any;
+  private readonly robotsParser: any;
+  private readonly indexStore: any;
+
+  private readonly maxPages: number;
+  private readonly maxDepth: number;
+
+  constructor(options: Options) {
+    this.workerCount = options.workerCount ?? 10;
+
+    this.queue = new Queue<CrawlTask>();
+    this.visited = new Set<string>();
+    this.state = {
+      activeWorkers: 0,
+    };
+
+    this.pageFetcher = options.pageFetcher;
+    this.htmlParser = options.htmlParser;
+    this.linkExtractor = options.linkExtractor;
+    this.robotsParser = options.robotsParser;
+    this.indexStore = options.indexStore;
+
+    this.maxPages = options.maxPages ?? Infinity;
+    this.maxDepth = options.maxDepth ?? Infinity;
+  }
+
+  async crawl(startUrl: string): Promise<void> {
+    const startUrlObj = new URL(startUrl);
+    const baseDomain = startUrlObj.hostname;
+
+    await this.robotsParser.load(startUrl);
+
+    this.queue.push({
+      url: startUrl,
+      depth: 0,
+    });
+
+    const workers: Promise<void>[] = [];
+
+    for (let i = 0; i < this.workerCount; i++) {
+      const worker = new Worker({
+        queue: this.queue,
+        visited: this.visited,
+        state: this.state,
+        robotsParser: this.robotsParser,
+        pageFetcher: this.pageFetcher,
+        htmlParser: this.htmlParser,
+        linkExtractor: this.linkExtractor,
+        maxPages: this.maxPages,
+        maxDepth: this.maxDepth,
+        indexStore: this.indexStore,
+      });
+
+      workers.push(worker.run(i, baseDomain));
+    }
+
+    await Promise.all(workers);
+  }
+}
+
+export { Crawler };
