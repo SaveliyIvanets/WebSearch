8d2e73507ff98f9a9988d95faa8b90bcd9bd077d
Dinarchak <jjustdinar@gmail.com>
2026-09-15 14:59:53 +0300

    пайплайн нормализации

--- src/crawler/UrlNormalizer/pipeline/NormalizationPipeline.ts
+++ src/crawler/UrlNormalizer/pipeline/NormalizationPipeline.ts
@@ -1,5 +1,30 @@
-import { UrlNormalizerOptions } from '../UrlNormalizerBuilder.js';
+import { NormalizationContext } from './NormalizationContext.js';
+import { NormalizerStep } from './NormalizerStep.js';
 
-interface NormalizationContext {
-    options: 
+export class NormalizationPipeline {
+    constructor(private readonly steps: NormalizerStep[]) {}
+
+    execute(context: NormalizationContext): NormalizationContext | null {
+        let currentContext = context;
+
+        for (let i = 0; i < this.steps.length; i++) {
+            const step = this.steps[i];
+
+            try {
+                const result = step.process(context);
+
+                //пока нет шагов, возвращающих null
+                if (result === null) {
+                    return null;
+                }
+
+                currentContext = result;
+            } catch (error){
+                console.error(`Step ${step.constructor.name} failed:`, error);
+                return null;
+            }
+        }
+
+        return currentContext;
+    }
 }
