45d4487335362b94a0aa2fae6959b5bad6e6864a
Dinarchak <jjustdinar@gmail.com>
2026-09-15 15:08:05 +0300

    fix: ошибка импорта

--- src/crawler/UrlNormalizer/steps/FragmentRemovalStep.ts
+++ src/crawler/UrlNormalizer/steps/FragmentRemovalStep.ts
@@ -1,7 +1,7 @@
 import { NormalizerStep } from "../pipeline/NormalizerStep.js";
 import { NormalizationContext } from "../pipeline/NormalizationContext.js";
 
-export class FragmentNormalizationStep implements NormalizerStep {
+export class FragmentRemovalStep implements NormalizerStep {
     process(context: NormalizationContext): NormalizationContext {
         context.url.hash = '';
         return context;
